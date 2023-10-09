7c8cb81de9422751a894defac28030e1b749b6c0
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2023-10-09 16:06:33 +0530

    today  commit

--- app.js
+++ app.js
@@ -1,131 +1,137 @@
 const {
-    DynamoDBClient,
-    PutItemCommand,
-    ScanCommand,
-    UpdateItemCommand,
-  } = require('@aws-sdk/client-dynamodb');
-  const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
-  
-  const moment= require('moment')
+  DynamoDBClient,
+  PutItemCommand,
+  UpdateItemCommand,
+} = require('@aws-sdk/client-dynamodb');
+const { marshall } = require('@aws-sdk/util-dynamodb');
 
-  
-  const client = new DynamoDBClient();
-
-  const currentDate = Date.now();      // get the current date and time in milliseconds
-  const formattedDate = moment(currentDate).format('YYYY-MM-DD');    //formating date
+const moment = require('moment')
 
 
-  
-  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;   // dateformate YYYY-MM-DD
-  const nameRegex = /^[A-Za-z ]{3,32}$/;     // character limit min 3 to max 32
+const client = new DynamoDBClient();
 
-  // validating the each parameter 
-  const validatePostData = (employee) => {
-    if( employee.CertificationValidLastDate < employee.CertifiedDate ){
-      throw new Error('certificate last date cannot be less then certified date.');
-    }
-    if(!(dateRegex.test(employee.CertifiedDate) && dateRegex.test(employee.CertificationValidLastDate))){
-      throw new Error('Invalid date format.');
-    }
-    if(!(nameRegex.test(employee.CertificationAuthority.test) && nameRegex.test(employee.TechnologyName))){
-      throw new Error('Invalid data.');
-    }
+const currentDate = Date.now();      // get the current date and time in milliseconds
+const formattedDate = moment(currentDate).format('YYYY-MM-DD');    //formating date
+
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/;   // dateformate YYYY-MM-DD
+const nameRegex = /^[A-Za-z ]{3,32}$/;     // character limit min 3 to max 32
+
+// validating the each parameter 
+const validatePostData = (employee) => {
+  if (employee.CertificationValidLastDate < employee.CertifiedDate) {
+    return 'certificate last date cannot be less then certified date.';
   }
-  
-  // creating a employee certification data
-  const createEmpCertificate = async (event) => {
-    console.log("inside the add certification details method");
-    const response = { statusCode: 200 };
-    try {
-      const body = JSON.parse(event.body);
-      const certificateDetails = body.certificateDetails
+  if (!(dateRegex.test(employee.CertifiedDate) && dateRegex.test(employee.CertificationValidLastDate))) {
+    return 'Invalid date format.';
+  }
+  if (!(nameRegex.test(employee.CertificationAuthority.test) && nameRegex.test(employee.TechnologyName))) {
+    return 'Invalid data.';
+  }
+}
+
+// creating a employee certification data
+const createEmpCertificate = async (event) => {
+  console.log("inside the create certification details");
+  const response = { statusCode: 200 };
+  try {
+    const body = JSON.parse(event.body);
+    const certificateDetails = body.certificateDetails
     // Check for required fields
-    if (!body.certificateDetails.TechnologyName || !body.certificateDetails.CertificationAuthority || !body.certificateDetails.CertifiedDate || !body.certificateDetails.CertificationValidLastDate  || !body.certificateDetails.IsActive) {
-        throw new Error('Required fields are missing.');
-      }
-  
-      // Validate the incoming data
-      validatePostData(certificateDetails);
-  
-    
+    if (!body.certificateDetails.TechnologyName || !body.certificateDetails.CertificationAuthority || !body.certificateDetails.CertifiedDate || !body.certificateDetails.CertificationValidLastDate || !body.certificateDetails.IsActive) {
+      throw new Error('Required fields are missing.');
+    }
+
+    // Validate the incoming data
+    const validationError = validatePostData(certificateDetails);
+    if (validationError) {
+      response.statusCode =400;
+     response.body=JSON.stringify({
+       message: validationError,
+     })
+     throw new Error(validationError);
+   }
+
     const params = {
       TableName: process.env.DYNAMODB_TABLE_NAME,
       Item: marshall({
         empId: body.empId,
-        certificateDetails : {
-        TechnologyName: certificateDetails.TechnologyName,                     //certified technology name object
-        CertificationAuthority: certificateDetails.CertificationAuthority,
-        CertifiedDate: certificateDetails.CertifiedDate,
-        CertificationValidLastDate: certificateDetails.CertificationValidLastDate,
-        IsActive: certificateDetails.IsActive,                                 //required boolean
-        CreatedDateTime: formattedDate, 
-        //UpdatedDateTime: 
-      }}, { removeUndefinedValues: true }),                                    //for remove undefined fields
+        certificateDetails: {
+          TechnologyName: certificateDetails.TechnologyName,                     //certified technology name object
+          CertificationAuthority: certificateDetails.CertificationAuthority,
+          CertifiedDate: certificateDetails.CertifiedDate,
+          CertificationValidLastDate: certificateDetails.CertificationValidLastDate,
+          IsActive: certificateDetails.IsActive,                                 //required boolean
+          CreatedDateTime: formattedDate,
+          //UpdatedDateTime: 
+        }
+      }, { removeUndefinedValues: true }),                                    //for remove undefined fields
     };
-    const createResult=await client.send(new PutItemCommand(params));
+    const createResult = await client.send(new PutItemCommand(params));
     response.body = JSON.stringify({
       message: 'Successfully created certificate details.',
       createResult,
     });
-     } catch (e) {
-      console.error(e);
-      response.statusCode = 500;
-      response.body = JSON.stringify({
-        message: 'Failed to certificate details.',
-        errorMsg: e.message,
-        errorStack: e.stack,
-      });
-    }
-    return response;
-  };
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: 'Failed to certificate details.',
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  return response;
+};
 
-  // updating the employee certification details by employee id
-  const updateEmpCertificate = async (event) => {
-    const response = { statusCode: 200 };
-    try {
-      const body = JSON.parse(event.body);
-      const objKeys = Object.keys(body);
-      const params = {
-        TableName: process.env.DYNAMODB_TABLE_NAME,
-        Key: marshall({ empId: event.pathParameters.empId }),
-        UpdateExpression: `SET ${objKeys
-          .map((_, index) => `#key${index} = :value${index}`)
-          .join(', ')}`,
-        ExpressionAttributeNames: objKeys.reduce(
+// updating the employee certification details by employee id
+const updateEmpCertificate = async (event) => {
+  const response = { statusCode: 200 };
+  try {
+    const body = JSON.parse(event.body);
+    const certificateDetails = body.certificateDetails
+    validatePostData(certificateDetails);
+    const objKeys = Object.keys(body);
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ empId: event.pathParameters.empId }),
+      UpdateExpression: `SET ${objKeys
+        .map((_, index) => `#key${index} = :value${index}`)
+        .join(', ')}`,
+      ExpressionAttributeNames: objKeys.reduce(
+        (acc, key, index) => ({
+          ...acc,
+          [`#key${index}`]: key,
+        }),
+        {}
+      ),
+      ExpressionAttributeValues: marshall(
+        objKeys.reduce(
           (acc, key, index) => ({
             ...acc,
-            [`#key${index}`]: key,
+            [`:value${index}`]: body[key],
           }),
           {}
-        ),
-        ExpressionAttributeValues: marshall(
-          objKeys.reduce(
-            (acc, key, index) => ({
-              ...acc,
-              [`:value${index}`]: body[key],
-            }),
-            {}
-          )
-        ),
-      };
-      const updateResult = await client.send(new UpdateItemCommand(params));
-      response.body = JSON.stringify({
-        message: 'Successfully updated certificate details.',
-        updateResult,
-      });
-    } catch (e) {
-      console.error(e);
-      response.statusCode = 500;
-      response.body = JSON.stringify({
-        message: 'Failed to update certificate details.',
-        errorMsg: e.message,
-        errorStack: e.stack,
-      });
-    }
-    return response;
-  };
-  
-  module.exports = {
-    createEmpCertificate,
-    updateEmpCertificate
-  };
+        )
+      ),
+    };
+    const updateResult = await client.send(new UpdateItemCommand(params));
+    response.body = JSON.stringify({
+      message: 'Successfully updated certificate details.',
+      updateResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: 'Failed to update certificate details.',
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+  return response;
+};
+
+module.exports = {
+  createEmpCertificate,
+  updateEmpCertificate
+};
--- test.js
+++ test.js
@@ -4,163 +4,114 @@ const {
   DynamoDBClient,
 } = require('@aws-sdk/client-dynamodb');
 const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
-
 // Mock DynamoDBClient to avoid making actual AWS calls
 const mockClient = {
   send: () => ({}),
 };
-
-// Mock employee data for createEmployeecertificate
-const createCertificateDetails = {
-    empId: "7",
-    certificateDetails: {
-        "TechnologyName":"AWS",
-        "CertificationAuthority": "hello",
-        "CertifiedDate": "2022-10-04",
-        "CertificationValidLastDate": "2023-10-05",
-        "IsActive":true
-    }
-  };
-
-
-// Mock employee data for updateEmployee
-const updateEmployeeData = {
-    "TechnologyName":"sagar",
-    "CertificationAuthority": "hello",
-    "CertifiedDate": "2022-10-05",
-    "CertificationValidLastDate": "2023-10-06",
-    "IsActive":true
+// Mock employee data for createEmployee
+const createEmployeeCertificateData = {
+  empId: "25",
+  certificateDetails: {
+    TechnologyName: "AWS",
+    CertificationAuthority: "hello",
+    CertifiedDate: "2022-10-04",
+    CertificationValidLastDate: "2023-10-05",
+    IsActive: true
+  }
 };
-
-// Successfully create an employee certification
+// Mock employee data for updateEmployee
+const updateEmployeeCertificateData = {
+  certificateDetails: {
+    TechnologyName: "AWr",
+    CertificationAuthority: "hello",
+    CertifiedDate: "2022-10-04",
+    CertificationValidLastDate: "2023-10-05",
+    IsActive: true
+  }
+};
+// Successfully create an employee
 describe('createEmployee unit tests', () => {
-    let originalDynamoDBClient;
-    before(() => {
-        originalDynamoDBClient = DynamoDBClient;
-        DynamoDBClient.prototype.send = () => mockClient.send();
-      });
-    after(() => {
-        DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
-      });
-    });
-
-    it('successfully create an employee certification', async () => {
-      // Mock event object with employee data
-      let event = {
-        body: JSON.stringify(createCertificateDetails),
-      };
-      const response = await createEmpCertificate(event);
-      expect(response.statusCode).to.equal(200);
-      const responseBody = JSON.parse(response.body);
-      expect(responseBody.message).to.equal('Successfully created certificate details.');  // Correct the message if necessary
-    });
-
-    it('fails to create an employee with missing data', async () => {
-      // Mock event object with missing data
-      const event = {
-        body: JSON.stringify({}), // Simulating missing required data in the request body
-      };
-    
-      // Call the createEmpCertificate function and await its response
-      const response = await createEmpCertificate(event);
-    
-      // Expecting an error response with a status code of 500
-      expect(response.statusCode).to.equal(500);
-    });
-    
-
-    it('fails to create an employee with invalid data', async () => {
-      // Mock event object with invalid data
-      const event = {
-        body: JSON.stringify({
-          // Invalid data that should fail validation
-          technologyName: 'sagar', // Example: Technology name is too short
-        }),
-      };
-    
-      // Call the createEmpCertificate function and await its response
-      const response = await createEmpCertificate(event);
-    
-      // Expecting an error response with a status code of 500
-      expect(response.statusCode).to.equal(500);
-    });
-    
-
-
-  // Successfully updated an employee
-  describe('Unit tests for updateEmployee', () => {
-    let originalDynamoDBClient;
-  
-    // Before running the tests, we temporarily override the DynamoDBClient's send method with a mockClient's send method.
-    before(() => {
-      originalDynamoDBClient = DynamoDBClient;
-      DynamoDBClient.prototype.send = () => mockClient.send(); // Mocking DynamoDBClient send method
-    });
-  
-    // After the tests, we restore the original send method to avoid affecting other parts of the code.
-    after(() => {
-      DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
-    });
-  
-    
-    it('successfully updates an employee', async () => {
-      // Mock event object with the employee ID and updated data
-      const event = {
-        pathParameters: {
-          empId: '2', // Assuming this empId exists
-        },
-        body: JSON.stringify(updateEmployeeData), // Provide the updated employee data
-      };
-    
-      // Call the updateEmpCertificate function and await its response
-      const response = await updateEmpCertificate(event);
-    
-      // Expecting a successful response with a status code of 200
-      expect(response.statusCode).to.equal(200);
-    
-      // Parse the response body to access its properties
-      const responseBody = JSON.parse(response.body);
-    
-      // Expecting a specific success message in the response body (adjust as needed)
-      expect(responseBody.message).to.equal('Successfully updated certificate details.');
-    });
-    
-
-
-    it('fails to update an employee with an invalid employee ID', async () => {
-      // Mock event object with an invalid employee ID
-      const event = {
-        pathParameters: {
-          empId: '8', // Assuming this empId does not exist or is invalid
-        },
-        body: JSON.stringify(updateEmpCertificate), // Provide the data for updating an employee
-      };
-    
-      // Call the updateEmpCertificate function and await its response
-      const response = await updateEmpCertificate(event);
-    
-      // Expecting an error response with a status code of 500
-      expect(response.statusCode).to.equal(500);
-    });
-    
-
-
-    it('fails to update an employee with invalid data', async () => {
-      // Mock event object with an employee ID and invalid data
-      const event = {
-        pathParameters: {
-          empId: '10',                      // Assuming this empId exists
-        },
-        body: JSON.stringify({
-          // Invalid data that should fail validation
-          CertifiedDate: "10-03-2023",         // Example: Technology name is too short
-        }),
-      };
-    
-      // Call the updateEmpCertificate function and await its response
-      const response = await updateEmpCertificate(event);
-    
-      // Expecting an error response with a status code of 500
-      expect(response.statusCode).to.equal(500);
-    });
+  let originalDynamoDBClient;
+  before(() => {
+    originalDynamoDBClient = DynamoDBClient;
+    DynamoDBClient.prototype.send = () => mockClient.send();
   });
+  after(() => {
+    DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
+  });
+  it('successfully create an employee', async () => {
+    // Mock event object with employee data
+    let event = {
+      body: JSON.stringify(createEmployeeCertificateData),
+    };
+    const response = await createEmpCertificate(event);
+    expect(response.statusCode).to.equal(200);
+    const responseBody = JSON.parse(response.body);
+    expect(responseBody.message).to.equal('Successfully created certificate details.');
+  });
+  // it('fails to create an employee with missing data', async () => {
+  //   // Mock event object with missing data
+  //   let event = {
+  //     body: JSON.stringify({}), // Missing required data
+  //   };
+  //   const response = await createEmployee(event);
+  //   expect(response.statusCode).to.equal(400); // Expecting a 400 Bad Request for missing data
+  // });
+  it('fails to create an employee with invalid certificate data', async () => {
+    // Mock event object with invalid data
+    let event = {
+      body: JSON.stringify({
+        certificateDetails : {
+        // Invalid data that should fail validation
+        TechnologyName: 'AB', // Too short
+        }
+      }),
+    };
+    const response = await createEmpCertificate(event);
+    expect(response.statusCode).to.equal(500); // Expecting a 400 Bad Request for invalid data
+    const responseBody = JSON.parse(response.body);
+    expect(responseBody.errorMsg).to.equal('Required fields are missing.');
+  });
+});
+// Successfully update an employee
+describe('updateEmployee unit tests', () => {
+  let originalDynamoDBClient;
+  before(() => {
+    originalDynamoDBClient = DynamoDBClient;
+    DynamoDBClient.prototype.send = () => mockClient.send();
+  });
+  after(() => {
+    DynamoDBClient.prototype.send = originalDynamoDBClient.prototype.send;
+  });
+  it('successfully update an employee certificate details', async () => {
+    // Mock event object with the employee ID and updated data
+    let event = {
+      pathParameters: {
+        empId: '2', // Assuming this postId exists
+      },
+      body: JSON.stringify(updateEmployeeCertificateData),
+    };
+    const response = await updateEmpCertificate(event);
+    expect(response.statusCode).to.equal(200);
+    const responseBody = JSON.parse(response.body);
+    expect(responseBody.message).to.equal('Successfully updated certificate details.'); // Update the message if necessary
+  });
+  it('fails to update an employee with invalid certificate data', async () => {
+    // Mock event object with invalid data
+    let event = {
+      pathParameters: {
+        empId: '25', // Assuming this postId exists
+      },
+      body: JSON.stringify({
+        // Invalid data that should fail validation
+        certificateDetails  : {
+          CertifiedDate: "2022-10-04", // invalid date format
+        }
+      }),
+    };
+    const response = await updateEmpCertificate(event);
+    expect(response.statusCode).to.equal(200); // Expecting a 400 Bad Request for invalid data
+    const responseBody = JSON.parse(response.body);
+    expect(responseBody.M).to.equal('Successfully updated certificate details.');
+  });
+ });
